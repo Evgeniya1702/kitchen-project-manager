4064edab3b75cd08069ee8b63119962995575b20
Evgeniya <didovichevgeniya@gmail.com>
2025-02-12 13:00:02 +0200

    Create models, improved admin panel and update migrations

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from catalog import models
+from catalog.models import Dish, DishType, Cook
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+    search_fields = ("name",)
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "dish_type",)
+    lister_filter = ("dish_type",)
+    search_fields = ("name", "description",)
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    model = Cook
+    fieldsets = list(UserAdmin.fieldsets) + [
+        (("Additional Information"), {"fields": ("year_of_experience",)})
+    ]
+    add_fieldsets = list(UserAdmin.fieldsets) + [
+        (("Additional Information"), {"fields": ("year_of_experience",)})
+    ]
+    list_display = ("id", "username", "email", "year_of_experience")
+    search_fields = ("username", "email",)
+
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    year_of_experience = models.IntegerField()
+    groups = models.ManyToManyField("auth.Group",
+                                    related_name="cooks",
+                                    blank=True,)
+    user_permissions = models.ManyToManyField("auth.Permission",
+                                              related_name="cook_user_permissions",
+                                              blank=True,)
+
+    def __str__(self):
+        return f"{self.username} ({self.year_of_experience} years exp)"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    def __str__(self):
+        return self.name
