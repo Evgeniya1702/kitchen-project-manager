a9af05e052015a0dfd1adc11c9fa1de3cbac05ff
Evgeniya <didovichevgeniya@gmail.com>
2025-02-12 13:37:42 +0200

    Create css styles

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from catalog.models import DishType, Dish, Cook
+
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    num_dishes = Dish.objects.count()
+    num_dishtypes = DishType.objects.count()
+    num_cook = Cook.objects.count()
+    context = {
+        "num_dishes": num_dishes,
+        "num_dishtypes": num_dishtypes,
+        "num_cook": num_cook,
+    }
+    return render(request, template_name='catalog/index.html', context=context,)
